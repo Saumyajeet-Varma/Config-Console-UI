aa2fc9974300f5db40790188081929b235503c93
Saumyajeet-Varma <saumyajeetv@gmail.com>
2026-04-06 00:50:34 +0530

    react-router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-export default function App() {
+import { Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+const App = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold text-white">
-        React + Tailwind init done
-      </h1>
+    <div className="p-10">
+
+      {/* Navbar */}
+      <nav className="flex gap-4 mb-6">
+        <Link to="/" className="text-blue-500">Home</Link>
+        <Link to="/login" className="text-blue-500">Login</Link>
+        <Link to="/register" className="text-blue-500">Register</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+
     </div>
-  );
+  )
 }
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="text-3xl font-bold">
+            Home Page
+        </div>
+    )
+}
+
+export default Home
